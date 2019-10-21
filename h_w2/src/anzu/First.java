7233fb945464ae7cb8f657a9515ce35cf01eb195
anazurcho <ana.zurabashvili.1@btu.edu.ge>
2019-10-21 09:51:16 +0400

    fix

--- h_w2/src/anzu/First.java
+++ h_w2/src/anzu/First.java
@@ -2,19 +2,32 @@ package anzu;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.List;
+import java.util.ArrayList;
 
 public class First {
 
-    public static void main(String[] args) throws IOException{
-	    int[] interval = {1,2};
-	    double y;
+	public static void main(String[] args) throws IOException{
+		double start = 100;
+		double finish = 200;
+		double i;
 
-	    BufferedWriter writer = new BufferedWriter(new FileWriter("First.txt", true));
-	    for (int x : interval) {
-	        y = Math.pow(x,2) + 2 * x + 3;
-	        writer.write(String.valueOf(y));
-	        writer.newLine();
-        }
-	    writer.close();
-    }
+		double y;
+
+		List<Double> arr = new ArrayList<>();
+
+		for (i=start; i<=finish; i+=1){
+			arr.add(i);
+		}
+
+		BufferedWriter writer = new BufferedWriter(new FileWriter("First.txt", true));
+
+		for (double x : arr){
+			x/=100;
+			y = Math.pow(x,2) + 2 * x + 3;
+			writer.write(String.valueOf(y));
+			writer.newLine();
+		}
+		writer.close();
+	}
 }
--- h_w2/src/anzu/Second.java
+++ h_w2/src/anzu/Second.java
@@ -1,4 +1,24 @@
 package anzu;
+import java.io.*;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Second {
+    public static void main(String[] args)throws Exception
+    {
+        File file = new File("First.txt");
+
+        BufferedReader br = new BufferedReader(new FileReader(file));
+
+        String st;
+        List<Double> minmax = new ArrayList<>();
+        while ((st = br.readLine()) != null){
+            minmax.add(Double.parseDouble(st));
+        }
+        br.close();
+        System.out.println("min is " + Collections.min(minmax));
+        System.out.println("min is " + Collections.max(minmax));
+    }
 }
