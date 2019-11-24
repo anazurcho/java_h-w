97bc5d0ba5bb724b0d0eddcb192f84f3d0bcd277
anazurcho <ana.zurabashvili.1@btu.edu.ge>
2019-11-24 22:01:17 +0400

    first h_w

---
+++ h_w3/src/anzu/First.java
@@ -0,0 +1,32 @@
+package anzu;
+import java.util.*;
+import java.io.*;
+
+
+public class First {
+
+    public void method1(){
+        ArrayList<Integer> arr = new ArrayList<>();
+        ArrayList<Integer> arr1 = new ArrayList<>();
+
+        Random rand = new Random();
+        for (int i=0; i<12; i++){
+            arr.add(rand.nextInt(100));
+        }
+        Collections.sort(arr);
+        System.out.println("თავდაპირველი");
+        System.out.println(Arrays.toString(arr.toArray()));
+        int i=0;
+        for(Integer listitem : arr){
+            if(i%2==0){
+                arr1.add(listitem);
+            }
+            i++;
+
+        }
+        System.out.println("ლუწი ინდექსის გარეშე ინდექსიანი");
+        System.out.println(Arrays.toString(arr1.toArray()));
+
+    }
+
+}
---
+++ h_w3/src/anzu/Main.java
@@ -0,0 +1,10 @@
+package anzu;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here(
+        First first = new First();
+        first.method1();
+    }
+}
